f1307dfd23d7a3155ab04e7ae8996dee4712c770
Mikeyy-300 <iandolimichela@gmail.com>
2026-07-25 15:20:52 +0200

    Update script.js
    open box

--- script.js
+++ script.js
@@ -4,11 +4,20 @@ const intro = document.getElementById("intro");
 
 const gift = document.getElementById("gift");
 
+const giftBox = document.getElementById("giftBox");
 
-button.addEventListener("click",()=>{
 
-    intro.style.display="none";
+button.addEventListener("click", () => {
+
+    intro.style.display = "none";
 
     gift.classList.remove("hidden");
+
+});
+
+
+giftBox.addEventListener("click", () => {
+
+    giftBox.classList.add("opened");
 
 });
