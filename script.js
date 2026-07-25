17e0cb53ae4a3e84e7f78ec282c97f45139e2892
Mikeyy-300 <iandolimichela@gmail.com>
2026-07-25 17:17:20 +0200

    Update script.js
    foto 2

--- script.js
+++ script.js
@@ -14,11 +14,15 @@ const photoRain = document.getElementById("photoRain");
 
 button.addEventListener("click", () => {
 
+
     intro.style.display = "none";
+
 
     gift.classList.remove("hidden");
 
+
 });
+
 
 
 
@@ -30,23 +34,27 @@ giftImage.addEventListener("click", () => {
 
     setTimeout(() => {
 
+
         giftImage.src = "images/gift-open.png";
 
-    },400);
+
+    }, 400);
 
 
 
     setTimeout(() => {
 
 
-        gift.style.display="none";
+        gift.style.display = "none";
+
 
         paypalScreen.classList.remove("hidden");
+
 
         createPhotoRain();
 
 
-    },1500);
+    }, 1500);
 
 
 });
@@ -54,32 +62,38 @@ giftImage.addEventListener("click", () => {
 
 
 
+
 function createPhotoRain(){
 
 
-    for(let i=0;i<40;i++){
+    for(let i = 0; i < 40; i++){
 
 
-        const img=document.createElement("img");
+        const img = document.createElement("img");
 
 
-        let number=Math.floor(Math.random()*10);
+        const number = Math.floor(Math.random()*10);
 
 
-        img.src=`images/${number}.png`;
+        img.src = `images/${number}.png`;
+
 
         img.classList.add("falling-photo");
 
 
-        img.style.left=Math.random()*100+"vw";
+        img.style.left = Math.random()*100 + "vw";
 
 
-        img.style.animationDuration=
-        (5+Math.random()*8)+"s";
+        img.style.animationDuration =
+        (6 + Math.random()*8) + "s";
 
 
-        img.style.animationDelay=
-        (-Math.random()*10)+"s";
+        img.style.animationDelay =
+        (-Math.random()*10) + "s";
+
+
+        img.style.width =
+        (50 + Math.random()*60) + "px";
 
 
         photoRain.appendChild(img);
